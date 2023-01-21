74989ae945f1dbbd493a4da669f36290ae022fd4
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-01-21 17:49:52 +0900

    создание пр-ия users, подключение его в settings, перенаправление запроса в шаблоны

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -21,5 +21,6 @@ app_name = 'posts'
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls'))
 ]
