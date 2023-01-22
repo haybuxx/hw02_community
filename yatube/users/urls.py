0d130a5f086fb15f95f61c75a10c7ff010dae993
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-01-22 12:42:34 +0900

    перопределил шаблоны

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,13 +1,50 @@
-from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.views import(LogoutView, LoginView,
+PasswordChangeDoneView, PasswordChangeView, PasswordResetCompleteView,
+PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView)
 from django.urls import path
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('logout/', LogoutView.as_view
-         (template_name='users/logged_out.html'), name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('login/', LoginView.as_view
-         (template_name='users/login.html'), name='login')
-] 
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='users/logged_out.html'),
+        name='logout'
+    ),
+    path(
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
+    path(
+        'password_change/done/', PasswordChangeDoneView.as_view(
+        template_name='users/password_change_done.html'),
+        name='password_change_done'
+    ),
+    path(
+        'password_change/', PasswordChangeView.as_view(
+        template_name='users/password_change_form.html'),
+        name='password_change'
+    ),
+    path('password_reset/complete/', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete'),
+        name='password_reset_complete'
+    ),
+    path(
+        'reset/<uidb>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm'
+        ),
+    path(
+        'password_reset/done/', PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'),
+        name='password_reset_done'
+        ),
+    path(
+        'password_reset/', PasswordResetView.as_view(
+        template_name='users/password_reset_form.html'),
+        name='password_reset_form'
+        )
+]
