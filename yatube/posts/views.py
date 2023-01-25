7b3539131168f11f7291be149e449e5b58fdcf29
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-01-25 15:27:51 +0900

    создание приложеня about, шаблонов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.core.paginator import Paginator
 
 COUNT_POST_PAGE = 10
 
 
 def index(request):
     posts = Post.objects.all()[:COUNT_POST_PAGE]
-    title = 'Последние обновления на сайте'
+    #title = 'Последние обновления на сайте'
+    #context = {
+    #    'posts': posts,
+    #    'title': title
+    #}
+    #return render(request, 'posts/index.html', context)
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
-        'title': title
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
